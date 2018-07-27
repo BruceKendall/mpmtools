75872ac86bd731b8a166ca39a14fceb8ea567397
Bruce Kendall <kendall@bren.ucsb.edu>
2018-07-27 15:49:16 -0600

    Set up to use datasets Caswell_Ex_2.1: create dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+### data.R
+### Documentation for datasets
+
+#' Example 2.1 from Caswell (2001)
+#'
+#' A 3 x 3 Leslie matrix model. Given as example 2.1, equation (2.8).
+#'
+#' @format A matrix with 3 rows and 3 columns
+#'
+#' @source Caswell, H. 2001. Matrix population models: Construction, analysis, and
+#' interpretation. 2nd edition. Sinauer Associates, Sunderland, MA.
+"Caswell_Ex_2.1"
---
+++ data-raw/Caswell-examples.R
@@ -0,0 +1,8 @@
+### Caswell-examples
+### Example datasets from Caswell (2001)
+
+# Example 2.1
+Caswell_Ex_2.1 <- matrix(c(0,   1,   5,
+                           0.3, 0,   0,
+                           0,   0.5, 0),
+                         3, 3, byrow = TRUE)
