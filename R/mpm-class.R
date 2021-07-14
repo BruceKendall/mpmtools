f96229cc6490bae6dde4ef04e6ceb78dc3103268
Bruce Kendall <kendall@bren.ucsb.edu>
2021-07-14 12:01:13 -0700

    Create mpm() with method for Leslie matrix

--- R/mpm-class.R
+++ R/mpm-class.R
@@ -56,3 +56,64 @@ new_mpm <- function(demog = list(births = numeric(),
             census = census,
             class = "mpm")
 }
+
+mpm <- function(x, matrix_type,
+                census = "post",
+                timestep = "year",
+                class_type = character(),
+                class_names = character(),
+                newborn_classes = character()) {
+  # Argument checking
+  # - x should be either a data frame (or tibble) or a list
+  # - matrix_type and census need to match a list of possible values
+  # - Lengths/dimensions of various objects should match
+  if (!is.list(x)) {
+    stop("x should be a data frame, tibble, or list")
+  }
+  if (!all(c("mx", "Px") %in% names(x))) {
+    stop("x must contain elements mx and Px")
+  }
+  # The following line should only apply to a standard birth-pulse model!
+  census <- match.arg(census, c("prebreeding", "postbreeding"))
+
+  # If not specified, extract sensible values for class_type, class_names,
+  #   and newborn_classes from information in x and matrix_type
+  if (length(class_type) == 0) {
+    class_type <- names(x)[1]
+    cat("`class_type` not specified, setting it to", class_type, "\n")
+  }
+  if (length(class_names) == 0) {
+    if (is.character(x[[class_type]])) {
+      class_names <- x[[class_type]]
+    } else {
+      class_names <- paste(class_type, x[[class_type]], sep = "_")
+    }
+    cat("`class_names` not specified, setting them to", class_names, "\n")
+  }
+  if (length(newborn_classes) == 0) {
+    newborn_classes <- class_names[1]
+    cat("`newborn_classes` not specified, setting it to", newborn_classes, "\n")
+  }
+  stopifnot(all(newborn_classes %in% class_names))
+
+  # Type-specific requirements
+  if (matrix_type == "Leslie") {
+    stopifnot(exprs = {
+      length(x$mx) == length(class_names)
+      length(x$Px) == length(class_names)
+      length(newborn_classes) == 1
+    })
+  }
+
+  # Construct demog
+  if (matrix_type == "Leslie") {
+    demog <- list(births = x$mx,
+                  survival = x$Px)
+  } else {
+    stop("Matrix type ", matrix_type, "not implemented!")
+  }
+
+  # Construct the mpm object
+  new_mpm(demog, timestep, matrix_type, class_type, class_names,
+          newborn_classes, census)
+}
